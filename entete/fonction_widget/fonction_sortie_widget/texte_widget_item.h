721e9bc1853a6ff24c6d4bbbdda3e014fe5860c2
sebangi <sebastien_angibaud@yahoo.fr>
2017-01-31 11:35:16 +0100

    amélioration dy texte_sortie

--- entete/fonction_widget/fonction_sortie_widget/texte_widget_item.h
+++ entete/fonction_widget/fonction_sortie_widget/texte_widget_item.h
@@ -1,35 +1,29 @@
 #ifndef TEXTE_WIDGET_ITEM_H
 #define TEXTE_WIDGET_ITEM_H
 
-#include <QTableWidgetItem>
-#include <QWidget>
-#include <QFrame>
+#include <QListWidgetItem>
 
 #include "entete/element/texte.h"
 
 class texte;
 
-class texte_widget_item : public QWidget, public  QTableWidgetItem
+class texte_widget_item :public QListWidgetItem
 {
-        Q_OBJECT
-
     private:
             texte & m_texte;
+            int m_num;
 
     public:
-            texte_widget_item(texte& t);
+            texte_widget_item(texte& t, int num);
             void update();
-            void paintEvent(QPaintEvent *);
 
             texte & get_texte() const;
 
-            /*
             inline int get_height() const
             {
                 QFontMetrics fm(this->font());
-                return m_texte.get_nb_lignes_avec_configuration() * (fm.lineSpacing() + 3);
+                return m_texte.get_nb_lignes_avec_information() * (fm.lineSpacing() + 3);
             }
-            */
 };
 
 #endif // TEXTE_WIDGET_ITEM_H
